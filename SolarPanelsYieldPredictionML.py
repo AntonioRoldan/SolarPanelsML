ca836227c31a25ce27ef23c55bdece0088dd7059
Antonio Roldán <antonioroldan95@outlook.com>
2025-07-29 09:08:16 +0200

    Code does not run but date time type issue was fixed

--- SolarPanelsYieldPredictionML.py
+++ SolarPanelsYieldPredictionML.py
@@ -262,9 +262,9 @@ print("\n \n")
 
 print(maxScaled.info())
 #Next three lines are necessary to fix this error "ValueError: DataFrame.dtypes for data must be int, float, bool or category. When categorical type is supplied, the experimental DMatrix parameter`enable_categorical` must be set to `True`.  Invalid columns:DATE_TIME: object, SENSOR_INVERTER_ID: object, SOLAR_PANEL_INVERTER_ID: object"
-maxScaled["DATE_TIME"] = pd.to_datetime(pd.Series(maxScaled["DATE_TIME"])).dt.strftime("%Y%m%d&H%M%S").astype(int)
-maxScaled["SENSOR_INVERTER_ID"].astype("category")
-maxScaled["SOLAR_PANEL_INVERTER_ID"].astype("category")
+maxScaled["DATE_TIME"] = pd.to_datetime(pd.Series(maxScaled["DATE_TIME"])).astype(int) / 10 * 9 #We turn date into seconds integer 
+maxScaled["SENSOR_INVERTER_ID"] = maxScaled["SENSOR_INVERTER_ID"].astype("category")
+maxScaled["SOLAR_PANEL_INVERTER_ID"] = maxScaled["SOLAR_PANEL_INVERTER_ID"].astype("category")
 
 X, y = maxScaled.drop('TOTAL_YIELD', axis=1), maxScaled[['TOTAL_YIELD']]
 # Splitting
